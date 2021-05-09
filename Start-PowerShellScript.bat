700d6eccbb51f33787e7415571267f4b0ad9c6fa
Fedor Klyanchin <46520425+fedor-klyanchin@users.noreply.github.com>
2021-05-09 17:10:24 +0300

    Update Start-PowerShellScript.bat

--- Start-PowerShellScript.bat
+++ Start-PowerShellScript.bat
@@ -1,11 +1,15 @@
 REM Runs a script PowerShell from any location
+REM This batch file must have the same name as the script file.
 @echo off
 chcp 1251
-REM Restart the script in background. Comment out the next line if not needed
+cls
+REM Restarts the script in the background.
 if "%~1" == "" (start "" /min "%comspec%" /c "%~f0" any_word & exit /b)
-REM The location of the script to run
-set LocationScript=TestFolder\TestScript.ps1
-REM Get current location
-set CurrentLocation=%~dp0
+REM The complete name and extension of the batch file
+set CompleteName="%~0"
+set Extension=%~x0
+REM Replace the extension
+call set LocationScript=%%CompleteName:%Extension%=.ps1%%
 REM Run the script PowerShell
-powershell.exe -NoLogo -NoExit -WindowStyle Normal -ExecutionPolicy Bypass -File "%CurrentLocation%%LocationScript%"
+powershell.exe -NoLogo -NoExit -WindowStyle Normal -ExecutionPolicy Bypass -File %LocationScript%
+set /p set=
